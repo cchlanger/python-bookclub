cecd055b12bcb193ca9a52b952a2d3296985bb33
Petrovic <mina.petrovic@imba.oeaw.ac.at>
2019-05-28 21:22:40 +0200

    Excercises 14-20 added

--- 101 python excercises_part1_MP.py
+++ 101 python excercises_part1_MP.py
@@ -32,3 +32,34 @@ a = np.array([1,2,3,2,3,4,3,4,5,6])
 b = np.array([7,2,10,2,7,4,9,4,9,8])
 c= np.where(a==b)
 c #still couldn't find the proper function
+#14. Q. Get all items between 5 and 10 from a.
+a = np.array([2, 6, 1, 9, 10, 3, 27])
+b = np.where((a>=5) & (a<=10))
+a[b]
+#15. Convert the function maxx that works on two scalars, to work on two arrays.
+def maxx(x, y):
+    """Get the maximum of two items"""
+    if x >= y:
+        return x
+    else:
+        return y
+pair_max = np.vectorize (maxx, otypes=[float])
+a = np.array([5, 7, 9, 8, 6, 4, 5])
+b = np.array([6, 3, 4, 8, 9, 7, 1])
+pair_max(a,b)
+#16. Swap columns 1 and 2 in the array arr
+arr = np.arange(9).reshape(3,3)
+arr
+arr[:, (1,0,2)]
+#17. Swap rows 1 and 2 in the array arr
+arr = np.arange(9).reshape(3,3)
+arr[(1,0,2),:]
+#18. Reverse the rows of a 2D array arr
+arr = np.arange(9).reshape(3,3)
+arr[(2,1,0),:]#my solution
+arr[::-1]#their solution
+#19. Reverse the columns of a 2D array arr.
+arr[:,(2,1,0)] #my solution
+arr[:, ::-1] #their solution
+#20. Create a 2D array of shape 5x3 to contain random decimal numbers between 5 and 10
+rand_arr = np.random.randint(low=5, high=10, size=(5,3)) + np.random.random((5,3))
